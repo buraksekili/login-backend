0a17a9773b46f8dde267c8ba6601b6825a1defc2
buraksekili <buraksekili@gmail.com>
2020-12-17 22:31:41 +0300

    Changed SQL queries for new db

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql2/promise");
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = require("../config");
+const fs = require("fs");
 
 // Set the connection config object
 const connectionConfig = {
@@ -10,11 +11,13 @@ const connectionConfig = {
   multipleStatements: true,
 };
 
-const login = async (mail, password) => {
+const login = async (mail, password, title) => {
   try {
     const connection = await mysql.createConnection(connectionConfig);
-    const SQL = `SELECT * FROM user WHERE user_email = ? and user_password = ?`;
+    const SQL = `SELECT * FROM loginners WHERE LoginnerMail = ? and LoginnerPassword = ?`;
     const [rows] = await connection.execute(SQL, [mail, password]);
+
+    console.log(`logging user with ${mail} - ${password} - ${title}`);
 
     if (!rows) {
       return ["The result is empty. Try again", undefined];
@@ -22,17 +25,27 @@ const login = async (mail, password) => {
     if (rows.length == 0) {
       return [`Invalid credentials`, undefined];
     }
+    if (!title || title == "" || title.length == 0) {
+      return ["Title field is empty for /login body.", undefined];
+    }
+
+    console.log("rows are", rows);
     return [undefined, rows];
   } catch (error) {
     return [error.message, undefined];
   }
 };
 
-const signup = async (mail, password) => {
+const signup = async (mail, password, phone, title) => {
   try {
     const connection = await mysql.createConnection(connectionConfig);
+
+    console.log(
+      `creating user with ${mail} - ${password} - ${phone} - ${title} `
+    );
+
     // check if the email exists or not.
-    let SQL = "SELECT * FROM user WHERE user_email = ?";
+    let SQL = "SELECT * FROM loginners WHERE LoginnerMail = ?";
     let [rows] = await connection.execute(SQL, [mail]);
 
     if (!rows) {
@@ -42,8 +55,9 @@ const signup = async (mail, password) => {
       return [`${mail} already exists.`, undefined];
     }
 
-    SQL = "INSERT INTO user(user_email, user_password) VALUES(?, ?)";
-    [rows] = await connection.execute(SQL, [mail, password]);
+    SQL =
+      "INSERT INTO loginners(LoginnerMail, LoginnerPassword, Phone, Title) VALUES(?, ?, ?, ?)";
+    [rows] = await connection.execute(SQL, [mail, password, phone, title]);
 
     if (!rows) {
       return ["The result is empty. Try again", undefined];
@@ -57,4 +71,36 @@ const signup = async (mail, password) => {
   }
 };
 
-module.exports = { login, signup };
+const insertImage = async () => {
+  try {
+    const connection = await mysql.createConnection(connectionConfig);
+
+    var onlyPath = require("path").dirname(
+      "C:\\Users\\ASUS\\Downloads\\logo.png"
+    );
+
+    console.log(`path is ${onlyPath + "\\logo.png"}`);
+
+    let SQL = `INSERT INTO Categories (CategoryName, CategoryImage, Active_Passive)  VALUES ('Beverages' LOAD_FILE('?') ,1)`;
+    var query = "INSERT INTO `Categories` SET ?",
+      values = {
+        file_type: "img",
+        file_size: buffer.length,
+        file: buffer,
+      };
+    let [rows] = await connection.execute(SQL, [onlyPath]);
+
+    if (!rows) {
+      return ["The result is empty. Try again", undefined];
+    }
+    if (rows.length != 0) {
+      return [`already exists.`, undefined];
+    }
+
+    return [undefined, rows];
+  } catch (error) {
+    return [error.message, undefined];
+  }
+};
+
+module.exports = { login, signup, insertImage };
--- src/routes/connection-route.js
+++ src/routes/connection-route.js
@@ -1,23 +1,24 @@
 const express = require("express");
-const { login, signup } = require("../db");
+const { login, signup, insertImage } = require("../db");
 
 const connectionRouter = express.Router();
 
 connectionRouter.post("/login", async (req, res) => {
   if (req.body && req.body[0]) {
-    const { mail, password } = req.body[0];
+    const { mail, password, title } = req.body[0];
 
-    const [error, response] = await login(mail, password);
+    const [error, response] = await login(mail, password, title);
 
     if (error) {
-      return res.status(401).json({ auth: false, error });
+      return res.status(401).json({ status: false, error });
     }
 
     if (response && response.length > 0) {
       return res.json({
-        auth: true,
-        user_id: response[0].user_id,
-        user_mail: response[0].user_email,
+        status: true,
+        user_id: response[0].ID,
+        user_mail: response[0].LoginnerMail,
+        user_title: response[0].Title,
       });
     }
   }
@@ -26,17 +27,34 @@ connectionRouter.post("/login", async (req, res) => {
 
 connectionRouter.post("/signup", async (req, res) => {
   if (req.body && req.body[0]) {
-    const { mail, password } = req.body[0];
+    const { mail, password, phone, title } = req.body[0];
+    // Insert incoming user into db.
+    const [error, response] = await signup(mail, password, phone, title);
 
-    const [error, response] = await signup(mail, password);
-
+    // occurred error while inserting a logginer into db.
     if (error) {
       return res.status(400).json({ error, status: false });
     }
 
+    // if we have response from the db.
     if (response && response.affectedRows && response.affectedRows > 0) {
       return res.json({ message: `${mail} is created`, status: true });
     }
+  }
+  // invalid json body.
+  return res.status(401).json({ error: "Invalid request body." });
+});
+
+connectionRouter.get("/insertsm", async (req, res) => {
+  const [error, response] = await insertImage();
+
+  if (error) {
+    return res.status(400).json({ error, status: false });
+  }
+
+  if (response && response.affectedRows && response.affectedRows > 0) {
+    console.log(response);
+    return res.json({ message: `is created`, status: true });
   }
   return res.status(401).json({ error: "Invalid request body." });
 });
